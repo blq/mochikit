44c89e082162e9f6925e80bc72ac90d857ea7971
blq <fblomqvist@gmail.com>
2010-10-25 01:23:42 +0200

    Script example on how to compile a subset of MochiKit using the Google Closure compiler. Including couple of necessary extern files to get MochiKit to compile, see http://code.google.com/closure/compiler/docs/api-tutorial3.html

---
+++ scripts/closure_compile.bat
@@ -0,0 +1,24 @@
+@echo off
+REM example compilation of MochiKit using the Google Closure compiler, http://code.google.com/p/closure-compiler/
+REM note that not all MochiKit modules are included yet
+java -jar Closure/compiler.jar ^
+	--compilation_level SIMPLE_OPTIMIZATIONS ^
+	--warning_level VERBOSE ^
+	--summary_detail_level 3 ^
+	^
+	--js_output_file ../packed/MochiKit/mochikit.cmp.js ^
+	^
+	--js ../MochiKit/Base.js ^
+	--js ../MochiKit/Async.js ^
+	--js ../MochiKit/Iter.js ^
+	--js ../MochiKit/DOM.js ^
+	--js ../MochiKit/Style.js ^
+	--js ../MochiKit/Signal.js ^
+	--js ../MochiKit/DateTime.js ^
+	--js ../MochiKit/Format.js ^
+	--js ../MochiKit/Text.js ^
+	--js ../MochiKit/Logging.js ^
+	^
+	--externs closure_externs/mochikit_extern.js ^
+	--externs closure_externs/webkit_console.js ^
+	--externs closure_externs/json.js
---
+++ scripts/closure_externs/mochikit_extern.js
@@ -0,0 +1,26 @@
+/**
+ * necessary to get MochiKit to compile
+ *
+ * @externs
+ */
+
+var MochiKit = {};
+
+
+//-- not part of MochiKit but needed..
+
+var Debug = {};
+var debug = {};
+
+//var console;
+
+var StopIteration = {};
+
+
+//---------
+
+/**
+ * test. just to reduce warnings
+ * @typedef {number}
+ */
+//var Int;
