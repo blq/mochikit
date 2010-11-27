693dd43b2b8adebb5a22647b1b4b40916036cb29
blq <fblomqvist@gmail.com>
2010-11-27 21:36:48 +0100

    minor code-style edits

--- MochiKit/Text-ext.js
+++ MochiKit/Text-ext.js
@@ -18,6 +18,7 @@ MochiKit.Base.module(MochiKit, 'Text_ext', '1.5', ['Text']);
 /**
  * Compute Levenshtein distance between two strings.
  * @see http://en.wikipedia.org/wiki/Levenshtein_distance
+ * @see http://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance
  *
  * based on code by Lasse Johansen and Anders Sewerin Johansen
  * note how this could be viewed as a specific case of the more general Wagner-Fisher distance.
@@ -32,11 +33,10 @@ MochiKit.Base.module(MochiKit, 'Text_ext', '1.5', ['Text']);
  * @param {boolean=} [allowTransposition=false] optional, default false
  * @return {number} distance between the two strings. The larger the number, the bigger the difference.
  */
-MochiKit.Text.levenshteinDistance = function(s, t, allowTransposition) // rename/alias as simply LD?
+MochiKit.Text.levenshteinDistance = function(s, t, allowTransposition)
 {
 	allowTransposition = allowTransposition || false;
-	var n = s.length;
-	var m = t.length;
+	var n = s.length, m = t.length;
 
 	if (n == 0) return m;
 	if (m == 0) return n;
@@ -46,16 +46,13 @@ MochiKit.Text.levenshteinDistance = function(s, t, allowTransposition) // rename
 	var d = new Array(n + 1);
 	for (var r = 0; r < n + 1; ++r)
 		d[r] = new Array(m + 1);
-
 	for (var i = 0; i <= n; ++i)
 		d[i][0] = i;
 	for (var j = 0; j <= m; ++j)
 		d[0][j] = j;
 
-	for (var i = 1; i <= n; ++i)
-	{
-		for (var j = 1; j <= m; ++j)
-		{
+	for (var i = 1; i <= n; ++i) {
+		for (var j = 1; j <= m; ++j) {
 			var cost = (t[j - 1] == s[i - 1] ? 0 : 1);
 
 			// min of three
@@ -64,24 +61,20 @@ MochiKit.Text.levenshteinDistance = function(s, t, allowTransposition) // rename
 				d[i][j - 1] + 1),
 				d[i - 1][j - 1] + cost
 			);
-
 			// Optional step: Cover transposition, in addition to deletion,
 			// insertion and substitution. This step is taken from:
 			// Berghel, Hal ; Roach, David : "An Extension of Ukkonen's
 			// Enhanced Dynamic Programming ASM Algorithm"
 			// (http://www.acm.org/~hlb/publications/asm/asm.html)
-			if (allowTransposition && (i > 2 && j > 2))
-			{
+			if (allowTransposition && (i > 2 && j > 2))	{
 				var trans = d[i - 2][j - 2] + 1;
 				if (s[i - 2] != t[j - 1]) ++trans;
 				if (s[i - 1] != t[j - 2]) ++trans;
 				if (cell > trans) cell = trans;
 			}
-
 			d[i][j] = cell;
 		}
 	}
-
 	return d[n][m];
 };
 
@@ -104,8 +97,7 @@ MochiKit.Text.humanStringCompare = function(a, b)
 	var reNum = /^(\+|\-)?\d+/;
 	var reTxt = /^\D+/; // inverse of reNum
 
-	while (a.length > 0 && b.length > 0)
-	{
+	while (a.length > 0 && b.length > 0) {
 		var a0 = null, ainc = -1;
 		var b0 = null, binc = -1;
 
@@ -120,7 +112,6 @@ MochiKit.Text.humanStringCompare = function(a, b)
 			binc = b0.length;
 		} else {
 			// possibly mixed string vs number case
-
 			if (ma == null) {
 				ma = a.match(reNum); // this _will_ now match (empty strings not possible either)
 				a0 = parseInt(ma[0], 10) + 2 << 15; // make any number bigger than any char (unicode)
