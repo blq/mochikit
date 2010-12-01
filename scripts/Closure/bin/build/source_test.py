0dbe275b174e5f750005ad2e245d6d10f5442124
blq <fblomqvist@gmail.com>
2010-12-01 11:49:20 +0100

    minor updates to sync with Google SVN (just updates to copyright text..)

--- scripts/Closure/bin/build/source_test.py
+++ scripts/Closure/bin/build/source_test.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 #
-# Copyright 2010 Google Inc. All Rights Reserved.
+# Copyright 2010 The Closure Library Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 """Unit test for source."""
@@ -25,6 +37,21 @@ class SourceTestCase(unittest.TestCase):
                      set(['goog.dom', 'goog.events.EventType']))
 
 
+  def testSingleLineCommentParse(self):
+    test_source = source.Source("//goog.provide('should.not.be.found');")
+    self.assertEqual(set([]), test_source.provides)
+
+
+  # currently this fails. see: #100, http://code.google.com/p/closure-library/issues/detail?id=100
+  def testMultilineCommentParse(self):
+    test_source = source.Source("""/*
+        goog.provide('should.not.be.found');
+        */
+    """)
+    self.assertEqual(set([]), test_source.provides)
+
+
+
 _TEST_SOURCE = """// Fake copyright notice
 
 /** Very important comment. */
@@ -39,6 +66,7 @@ function foo() {
   // Set bar to seventeen to increase performance.
   this.bar = 17;
 }
+
 """
 
 if __name__ == '__main__':
