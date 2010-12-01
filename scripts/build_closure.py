8c53a6b5536fa56b5945ca1edac75d0f57799d80
blq <fblomqvist@gmail.com>
2010-12-01 13:53:25 +0100

    updated to generate a complete build. As an example it also separates the build in two packages, the origonal core MochiKit + this fork's extensions.

--- scripts/build_closure.py
+++ scripts/build_closure.py
@@ -3,6 +3,9 @@
 #
 # @author Fredrik Blomqvist
 #
+# @see http://code.google.com/closure/library/docs/closurebuilder.html
+#
+#
 
 import subprocess
 
@@ -13,24 +16,55 @@ import make_closure_deps
 if __name__ == '__main__':
 
   # list modules (not file names!) you want to build here
-  compile_modules = [ 'MochiKit.Base', 'MochiKit.Signal', 'MochiKit.Iter', 'MochiKit.Base_ext', 'MochiKit.Bisect', 'MochiKit.HeapQ', 'MochiKit.Iter_ext' ]
+  # todo: should rather parse the MochiKit.js file (as pack.py does) but this way we be more illustrative
 
+  mochikit_core_modules = [
+    'MochiKit.' + mod
+    for mod in [
+        "Base",
+        "Iter",
+        "Logging",
+        "DateTime",
+        "Format",
+        "Text",
+        "Async",
+        "DOM",
+        "Selector",
+        "Style",
+        "LoggingPane",
+        "Color",
+        "Signal",
+        "Position",
+        "Visual",
+        "DragAndDrop",
+        "Sortable"
+    ]
+  ]
 
-  # example cmd line (todo: extract params as configs)
-  p = subprocess.Popen(['python', 'Closure/bin/build/modulebuilder.py',
+  modules = [
+    ('MochiKit', mochikit_core_modules),
+    ('MochiKit_blq_fork_extensions', [ 'MochiKit.Base_ext', 'MochiKit.Bisect', 'MochiKit.HeapQ', 'MochiKit.Iter_ext', 'MochiKit.Random' ])
+  ]
+
+  mod_args = []
+  for mod in modules:
+    mod_args.extend([ '--module', mod[0] + ':' + ','.join(mod[1]) ])
+
+  args = [
     '--compiler_jar', 'Closure/compiler.jar',
     '--output_mode', 'compiled',
-    '--root', '../MochiKit',
-    '--module', 'MochiKit:' + ','.join(compile_modules),
-    '--list_unused_files', 'true',
+    '--root', '../MochiKit'
+    ] + mod_args + [
+    '--list_unused_files', 'true', # list orphan files not picked up by the module spec. for debug/testing
+    '--keep_base_separate', 'true', # whether you'd like goog Closure base.js baked in or include it separately
     # ---
     # cmd line for actual compiler. must be a separate string
     # todo: enable VERBOSE mode once last issues in code have been fixed - this is where compiler really does its job!
+    # todo: unless you compile MochiKit *toghether*  with your app scripts, ADVANCED_OPTIMIZATION can't (currently) be used, need to "export" the API functions.
     '--compiler_flags', '\
       --compilation_level SIMPLE_OPTIMIZATIONS\
       --warning_level DEFAULT\
       --summary_detail_level 3\
-      --formatting PRETTY_PRINT\
       --jscomp_off nonStandardJsDocs\
       --process_closure_primitives true\
       \
@@ -43,7 +77,11 @@ if __name__ == '__main__':
       --externs closure_externs/generic_types.js\
       --externs closure_externs/webkit_console.js\
       --externs closure_externs/json.js'
-  ])
+  ]
+
+  # example cmd line (todo: extract params as configs)
+  # our own tool, similar to Google's closurebuilder.py but let's you work with *module(s)*, not files.
+  p = subprocess.Popen(['python', 'Closure/bin/build/modulebuilder.py'] + args)
 
   # generate dependency file
   make_closure_deps.main()
