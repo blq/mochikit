1cd682bfd13df260d45b2045bcbadf5839c53249
blq <fblomqvist@gmail.com>
2010-12-05 02:53:50 +0100

    Added unit tests for Iter.interleave, combinations, compressIter. Improved test isolation.

--- tests/test_Iter_ext.js
+++ tests/test_Iter_ext.js
@@ -59,6 +59,7 @@ tests.test_Iter_ext = function (t) {
 
 		t.is(MochiKit.Base.compare(nodeValues, [ 1, 3, 6, 5, 7, 2, 4 ]), 0, 'treePreOrderIter');
 	}
+	testTreePreOrderIter();
 
 	function testTreeLevelOrderIter()
 	{
@@ -74,6 +75,7 @@ tests.test_Iter_ext = function (t) {
 
 		t.is(MochiKit.Base.compare(nodeValues, [ 1, 2, 3, 4, 5, 6, 7 ]), 0, 'treeLevelOrderIter');
 	}
+	testTreeLevelOrderIter();
 
 	function testTreePostOrderIter()
 	{
@@ -89,6 +91,7 @@ tests.test_Iter_ext = function (t) {
 
 		t.is(MochiKit.Base.compare(nodeValues, [ 6, 7, 5, 3, 4, 2, 1 ]), 0, 'treePostOrderIter');
 	}
+	testTreePostOrderIter();
 
 	function testPairIter()
 	{
@@ -116,6 +119,7 @@ tests.test_Iter_ext = function (t) {
 		t.is(MochiKit.Base.compare(values, []), 0, 'pairIter empty range');
 */
 	}
+	testPairIter();
 
 	function testWindowIter()
 	{
@@ -134,45 +138,89 @@ tests.test_Iter_ext = function (t) {
 
 		t.is(MochiKit.Base.compare(singlePair, [ [1, 2] ]), 0, 'windowView(singlePair)');
 	}
-
-	// run tests
-	testTreePreOrderIter();
-	testTreeLevelOrderIter();
-	testTreePostOrderIter();
-	testPairIter();
 	testWindowIter();
+
+
 	// todo: leafParentIter
 
-	//---------
+	function test_flatten()
+	{
+		// copy from test_Base.js
+		var flat = list(iflattenArray([1, "2", 3, [4, [5, [6, 7], 8, [], 9]]]));
+		var expect = [1, "2", 3, 4, 5, 6, 7, 8, 9];
+		t.is( repr(flat), repr(expect), "iflattenArray" );
 
-	// copy from test_Base.js
-    var flat = list(iflattenArray([1, "2", 3, [4, [5, [6, 7], 8, [], 9]]]));
-    var expect = [1, "2", 3, 4, 5, 6, 7, 8, 9];
-    t.is( repr(flat), repr(expect), "iflattenArray" );
-
-	t.eq(list(MochiKit.Iter.iflattenArray([ [1], [4,[5]], [],[[[]]],[9], [[4]] ])), [1,4,5,9,4], 'iflattenArray ok');
+		t.eq(list(MochiKit.Iter.iflattenArray([ [1], [4,[5]], [],[[[]]],[9], [[4]] ])), [1,4,5,9,4], 'iflattenArray ok');
+	}
+	test_flatten();
 
 
-	t.eq(list(MochiKit.Iter.chainFromIter([ [1], [4,5], [9] ])), [1,4,5,9]);
-	t.eq(list(MochiKit.Iter.chainFromIter([ iter([1]), iter([4,5]), [9] ])), [1,4,5,9]);
+	function test_chainFromIter()
+	{
+		t.eq(list(MochiKit.Iter.chainFromIter([ [1], [4,5], [9] ])), [1,4,5,9]);
+		t.eq(list(MochiKit.Iter.chainFromIter([ iter([1]), iter([4,5]), [9] ])), [1,4,5,9]);
 
-	t.eq(list(chainFromIter(groupby([1,1,1,2,2,3,3]), function(v) { return v[1]; })), [1,1,1,2,2,3,3], 'indirectChain complements groupby');
-	//------
+		t.eq(list(chainFromIter(groupby([1,1,1,2,2,3,3]), function(v) { return v[1]; })), [1,1,1,2,2,3,3], 'indirectChain complements groupby');
+	}
+	test_chainFromIter();
 
-	t.eq( list(uniqueView([1,2,3,4])), [1,2,3,4]);
-	t.eq( list(uniqueView([1,1, 2,3,4,4,4])), [1,2,3,4]);
-	t.eq( list(uniqueView([1])), [1]);
-	t.eq( list(uniqueView([])), []);
-	t.eq( list(uniqueView([1,1,1,1])), [1]);
+
+	function test_uniqueView()
+	{
+		t.eq( list(uniqueView([1,2,3,4])), [1,2,3,4]);
+		t.eq( list(uniqueView([1,1, 2,3,4,4,4])), [1,2,3,4]);
+		t.eq( list(uniqueView([1])), [1]);
+		t.eq( list(uniqueView([])), []);
+		t.eq( list(uniqueView([1,1,1,1])), [1]);
+	}
+	test_uniqueView();
+
 
 	//----
 	t.eq( list(iproduct([1, 2, 3], ['a', 'b'])), [[1, 'a'], [1, 'b'], [2, 'a'], [2, 'b'], [3, 'a'], [3, 'b']], '');
 
-	//---
-	t.eq( list( izipLongest([]) ), [], '');
-	t.eq( list( izipLongest([ [] ]) ), [], '');
-	t.eq( list( izipLongest([ [1] ]) ), [[1]], '');
-	t.eq( list( izipLongest([ [1], [2] ]) ), [[1, 2]], '');
-	t.eq( list( izipLongest([ [1], [2, 3] ]) ), [[1, 2], [null, 3]], '');
+	function test_izipLongest()
+	{
+		t.eq( list( izipLongest([]) ), [], '');
+		t.eq( list( izipLongest([ [] ]) ), [], '');
+		t.eq( list( izipLongest([ [1] ]) ), [[1]], '');
+		t.eq( list( izipLongest([ [1], [2] ]) ), [[1, 2]], '');
+		t.eq( list( izipLongest([ [1], [2, 3] ]) ), [[1, 2], [null, 3]], '');
+	}
+	test_izipLongest();
+
+
+	function test_interleave()
+	{
+		t.eq(list(interleave([])), []);
+		t.eq(list(interleave([1])), [1]);
+		t.eq(list(interleave(['a', 'b', 'c'], [1, 2, 3], ['x', 'y', 'z'])), ['a', 1, 'x', 'b', 2, 'y', 'c', 3, 'z']);
+	}
+	test_interleave();
+
+	function test_compress()
+	{
+		t.eq(list(compressIter(['A','B','C','D','E','F'], [1,0,1,0,1,1])), ['A','C','E','F']);
+        t.eq(list(compressIter(['A','B','C','D','E','F'], [0,0,0,0,0,0])), []);
+        t.eq(list(compressIter(['A','B','C','D','E','F'], [1,1,1,1,1,1])), ['A','B','C','D','E','F']);
+        t.eq(list(compressIter(['A','B','C','D','E','F'], [1,0,1])), ['A','C']);
+        t.eq(list(compressIter(['A','B','C'], [0,1,1,1,1,1])), ['B','C']);
+
+        var n = 1000;
+        var data = chainFromIter(repeat(list(range(6)), n));
+		//var data = chainFromIter(repeat(xrange(6), n));
+        var selectors = chainFromIter(repeat([0, 1]));
+		t.eq(list(compressIter(data, selectors)), list(repeatSeq([1,3,5], n)));
+	}
+	test_compress();
+
+
+	function test_combinations()
+	{
+		t.eq(list(combinations(['A','B','C','D'], 2)), [['A','B'], ['A','C'], ['A','D'], ['B','C'], ['B','D'], ['C','D']]);
+        t.eq(list(combinations(range(4), 3)), [[0,1,2], [0,1,3], [0,2,3], [1,2,3]]);
+	}
+	test_combinations();
+
 
 };
