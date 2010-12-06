fc887b29cde64e2b827310ac9fca1c541a77dc16
blq <fblomqvist@gmail.com>
2010-12-06 13:00:48 +0100

    improved cmd-line generator for modulebuilder(prep. for options)

--- scripts/build_closure.py
+++ scripts/build_closure.py
@@ -15,40 +15,38 @@ import make_closure_deps
 #import Closure.bin.build.modulebuilder # our own tool
 
 
-
 def build_modules(modules):
   mod_args = []
   for mod in modules:
     mod_args.extend([ '--module', mod[0] + ':' + ','.join(mod[1]) ])
 
-  # example cmd line (todo: extract params as configs)
-  args = [
-    '--compiler_jar', 'scripts/Closure/compiler.jar',
-    '--output_mode', 'compiled',
-    '--root', 'MochiKit'
-    ] + mod_args + [
-    '--list_unused_files', 'true', # list orphan files not picked up by the module spec. for debug/testing
-    '--keep_base_separate', 'true', # whether you'd like goog Closure base.js baked in or include it separately
-    # ---
+  # generate example cmd line (todo: extract params as configs) (yes.. modulebuilder should rather expose better interface)
+  args = list(
+    ('--compiler_jar', 'scripts/Closure/compiler.jar') +
+    ('--output_mode', 'compiled') +
+    ('--root', 'MochiKit') +
+    ('--list_unused_files', 'true') + # list orphan files not picked up by the module spec. for debug/testing
+    ('--keep_base_separate', 'true')  # whether you'd like goog Closure base.js baked in or include it separately
+    ) + mod_args + list(
     # cmd line for actual compiler. must be a separate string
-    '--compiler_flags', ' '.join([
-        '--compilation_level', 'SIMPLE_OPTIMIZATIONS', # todo: unless you compile MochiKit *toghether*  with your app scripts, ADVANCED_OPTIMIZATION can't (currently) be used, need to "export" the API functions.
-        '--warning_level DEFAULT', # todo: enable VERBOSE mode once last issues in code have been fixed - this is where compiler really does its job!
-        '--summary_detail_level', '3',
-        '--jscomp_off', 'nonStandardJsDocs',
-        '--process_closure_primitives', 'true',
+    ('--compiler_flags', ' '.join(list(reduce(lambda x,y: x+y, [
+      ('--compilation_level', 'SIMPLE_OPTIMIZATIONS'), # todo: unless you compile MochiKit *toghether*  with your app scripts, ADVANCED_OPTIMIZATION can't (currently) be used, need to "export" the API functions.
+      ('--warning_level', 'DEFAULT'), # todo: enable VERBOSE mode once last issues in code have been fixed - this is where compiler really does its job!
+      ('--summary_detail_level', '3'),
+      ('--jscomp_off', 'nonStandardJsDocs'),
+      ('--process_closure_primitives', 'true'),
 
-        '--jscomp_warning', 'deprecated',
-        '--jscomp_warning', 'visibility',
-        '--jscomp_warning', 'accessControls',
-        '--jscomp_warning', 'strictModuleDepCheck',
+      ('--jscomp_warning', 'deprecated'),
+      ('--jscomp_warning', 'visibility'),
+      ('--jscomp_warning', 'accessControls'),
+      ('--jscomp_warning', 'strictModuleDepCheck'),
 
-        '--externs', 'scripts/closure_externs/mochikit_extern.js',
-        '--externs', 'scripts/closure_externs/generic_types.js',
-        '--externs', 'scripts/closure_externs/webkit_console.js',
-        '--externs', 'scripts/closure_externs/json.js'
-      ])
-  ]
+      ('--externs', 'scripts/closure_externs/mochikit_extern.js'),
+      ('--externs', 'scripts/closure_externs/generic_types.js'),
+      ('--externs', 'scripts/closure_externs/webkit_console.js'),
+      ('--externs', 'scripts/closure_externs/json.js')
+      ])))
+    ))
 
   # our own tool, similar to Google's closurebuilder.py but let's you work with *module(s)*, not files.
   subprocess.check_call(['python', 'scripts/Closure/bin/build/modulebuilder.py'] + args) # throws if build fails
